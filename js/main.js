3e5f764f2816adeb64c18ff287c625de0c3e0dcc
Muhammadqodir_57 <mirzamatovanasiba@gmail.com>
2024-01-01 21:04:48 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let StudentAge = prompt ('Yoshingizni kiriting');
+if (StudentAge < 18) {
+    console.log("You can't get driver's license");
+} else 
+    console.log("You can get driver's license");
+
+
